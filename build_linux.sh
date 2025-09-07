6ed79d3dfe0c3bd959f5a9f532df1853eed4e73f
WillyJL <me@willyjl.dev>
2025-09-07 02:45:24 +0200

    ........

--- build_linux.sh
+++ build_linux.sh
@@ -19,8 +19,5 @@ make install
 
 linuxdeploy --appdir=AppDir -o appimage \
     --custom-apprun="../installer-assets/appimage/AppRun" \
-    --library="$LIBSSL3_OVERRIDE"
-
-linuxdeploy-plugin-qt --appdir=AppDir -o appimage \
-    --custom-apprun="../installer-assets/appimage/AppRun" \
-    --library="$LIBSSL3_OVERRIDE"
+    --library="$LIBSSL3_OVERRIDE" \
+    --plugin qt
